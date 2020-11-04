9c25a97a421acd8fc94535b202ba762a5275c4c2
Cristian Montanari <montanari.cristian1@gmail.com>
2020-11-04 11:56:01 -0300

    .

--- Lab.Demo.EF.Logic/CategoryLogic.cs
+++ Lab.Demo.EF.Logic/CategoryLogic.cs
@@ -15,6 +15,7 @@ namespace Lab.Demo.EF.Logic
         #region Públicos
         public List<Categories> GetAll()
         {
+            int i = 0;
             var categories = context.Categories.ToList();
 
             if (categories.Count() > 0)
